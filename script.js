3eafb443b425acdcd60b3e7ad120fefc8f4f718f
Anonymoux <70268810+Olamil@users.noreply.github.com>
2025-10-15 10:34:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+body {
+  text-align: center;
+  background: #111;
+  color: white;
+  font-family: Arial, sans-serif;
+}
+
+canvas {
+  background: #222;
+  margin-top: 20px;
+  border: 2px solid white;
+}
 
